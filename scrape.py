a283574f86fa2953553b8e35034aa9e8575e3ec4
Abe <abe.kazemzadeh@gmail.com>
2015-11-05 09:05:55 -0800

    after 2nd hack night

--- scrape.py
+++ scrape.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python
+
 from bs4 import BeautifulSoup
+from hashlib import md5
+
 soup = BeautifulSoup(open("out.html"), 'html.parser')
+roi = None
 for x in soup.find_all('div'):
-  if "class" in x.attrs and "row-forecast" in x['class']:
-    print x.get_text()
+  # if "class" in x.attrs and "row-forecast" in x['class']:
+  #   print x.get_text()
+  if "id" in x.attrs and "detailed-forecast-body" in x['id']:
+    #print x.get_text()
+    #print x.head
+    roi = x
+    break
+
+m = md5()
+m.update(roi.text)
+m.digest()
